b76d6e7a65a7e707e5bb0374f26b62657f474d0b
ubaiddevpk <obaidullah.developer@gmail.com>
2025-08-17 15:33:17 +0500

    update in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -14,10 +13,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-     
-  },
-  server: {
-    host: true,
-    port: 5173,
   },
 });
